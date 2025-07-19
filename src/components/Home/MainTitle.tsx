eca972731aabc4e3cddc0c9fdb1c1cf8c043e3b5
Rishabh Pandey <rishabhpandey8092@gmail.com>
2025-07-19 23:52:17 +0530

    feat: Added Audience response

--- src/components/Home/MainTitle.tsx
+++ src/components/Home/MainTitle.tsx
@@ -1,15 +1,17 @@
 import { CreateFreeNoteButton } from "../Common/buttons/CreateFreeNote"
 import { WatchDemoButtom } from "../Common/buttons/WatchDemo"
+import { AudienceResponse } from "./AudienceResponse"
 
 export const MainTitle = () => {
     return (
-        <div className="h-fit w-fit px-5 text-center flex flex-col gap-5.5">
+        <div className="h-fit w-fit px-5 text-center flex flex-col items-center justify-center gap-5.5">
             <h1 className="text-[42px] leading-12 font-bold font-sans">Double Your Learning with NoteFlow<sup className="text-2xl">AI</sup></h1>
             <p className="text-base px-2 font-medium text-zinc-500 tracking-wide font-sans">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate maiores magni explicabo qui quod, quidem beatae ea quaerat! Nulla, dicta.</p>
             <div className="w-full py-5 h-fit flex flex-col gap-5 items-center justify-center">
                 <CreateFreeNoteButton />
                 <WatchDemoButtom />
             </div>
+            <AudienceResponse />
         </div>
     )
 }
