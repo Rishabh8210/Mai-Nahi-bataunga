53058ee8b8b5dd286047fcfdcd6087b53a33cf9d
Rishabh Pandey <rishabhpandey8092@gmail.com>
2025-07-20 05:43:56 +0530

    feat: Complete mobile responsive section 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { Navbar } from "@/components/Common/Navbar";
+import { FeaturePerformance } from "@/components/Home/FeaturePromotions";
 import { MainTitle } from "@/components/Home/MainTitle";
 
 export default function Home() {
   return (
-    <div className="min-h-screen h-auto w-screen max-w-screen flex flex-col">
+    <div className="min-h-screen h-auto w-screenflex flex-col">
       <Navbar />
       <section className="h-fit w-full flex flex-col py-12">
         <MainTitle />
+        <div className="h-fit  pt-10 w-full px-5">
+          <FeaturePerformance />
+        </div>
       </section>
     </div>
   );
